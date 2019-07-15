ebd5c524e948bf302531bbaac6749d22e9d614c0
LOUIS TANG (ITA-ISDC-ISD-OOCLL/ZHA) <TANGLO2@OOCL.COM>
2019-07-15 19:57:25 +0800

    add:some packages

--- src/test/java/com/tws/refactoring/extract_variable/PriceCalculatorTest.java
+++ src/test/java/com/tws/refactoring/extract_variable/PriceCalculatorTest.java
@@ -1,7 +1,7 @@
 package com.tws.refactoring.extract_variable;
 
+import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 
 class PriceCalculatorTest {
 
