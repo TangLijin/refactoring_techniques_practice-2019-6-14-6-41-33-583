a5f6645bfe06132c42bac3cba74b6254a06ee017
LOUIS TANG (ITA-ISDC-ISD-OOCLL/ZHA) <TANGLO2@OOCL.COM>
2019-07-15 17:53:36 +0800

    add:should_return_banner_on_Mac_when_renderBanner_given_String_Macta_and_re()

--- src/test/java/com/tws/refactoring/extract_variable/BannerRenderTest.java
+++ src/test/java/com/tws/refactoring/extract_variable/BannerRenderTest.java
@@ -1,19 +1,20 @@
 package com.tws.refactoring.extract_variable;
 
-import org.junit.Test;
+
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BannerRenderTest {
 
     @Test
-    void should_return_IE_on_Mac_when_renderBanner_given_String_Macta_and_rie(){
+    public void should_return_IE_on_Mac_when_renderBanner_given_String_Macta_and_rie(){
 //        given
         BannerRender bannerRender = new BannerRender();
         String platform = "Macta";
-        String brower = "ire";
-        String expected = "IE_on_Mac?";
+        String brower = "ier";
+        String expected = "IE on Mac?";
 
 //    when
         String result = bannerRender.renderBanner(platform,brower);
@@ -22,7 +23,20 @@ class BannerRenderTest {
         Assertions.assertEquals(result,expected);
     }
 
+    @Test
+    public void should_return_banner_on_Mac_when_renderBanner_given_String_Macta_and_re() {
+//        given
+        BannerRender bannerRender = new BannerRender();
+        String platform = "Macta";
+        String brower = "re";
+        String expected = "banner";
 
+//    when
+        String result = bannerRender.renderBanner(platform, brower);
 
+//    then
+        Assertions.assertEquals(result, expected);
+
+    }
 }
 
